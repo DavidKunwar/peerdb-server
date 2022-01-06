58883577803ed3c8a61abfe73069c58f5081d77e
David Kunwar <dkunwar7111@gmail.com>
2022-01-06 20:44:06 +0530

    Initial commit, crete auth backend TASK1

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+
+// app.get('/', function(req, res){
+//   res.send("Hello World")
+// })
+
+app.post('/login', function(req, res){
+
+  const username = req.body.username
+  const password = req.body.password
+
+  if(username === 'peerdb' && password === 'peerdb'){
+    res.send({isAuth: true})
+  }else{
+    res.send({isAuth: false})
+  }
+})
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, function(){
+  console.log(`Server is up and running on Port : ${PORT}`)
+})
